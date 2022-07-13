f8b93d0a5eb5c89e606a2f56283cea2c05f6a325
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-07-13 10:58:25 -0300

    Criacao do arquivo index.html e tetris.js

---
+++ tetris.js
@@ -0,0 +1,48 @@
+
+class Tetris {
+	constructor(imageX, imageY, template) {
+		this.imageY = imageY;
+		this.imageX = imageX;
+		this.template = template;
+	
+	}
+
+	checkBottom() {}
+
+	checkLeft() {}
+
+	moveRight() {}
+
+	moveLeft() {}
+
+	moveBottom() {}
+
+	changeRotation() {}
+}
+
+const imageSquareSize = 24;
+const size = 40;
+const framePerSecond = 24;
+const gameSpeed = 5;
+const canvas = document.getElementById("canvas");
+const image = document.getElementById("image");
+const ctx = canvas.getContext('2d');
+const squareCountX = canvas.width / size;
+const squareCountY = canvas.height / size;
+
+const shapes = [
+	new Tetris(0, 120, [
+		[0, 1, 0],
+		[0, 1, 0],
+		[1, 1, 0]
+	],
+]
+
+
+
+
+
+
+
+
+
