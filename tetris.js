7a2fc88f2dbc0692e6758b4bf6c636bda6dbe64e
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-07-13 16:03:59 -0300

    Atualizacao do arquivo tetris.js

--- tetris.js
+++ tetris.js
@@ -1,5 +1,12 @@
+/*
+ *  Data: 02 / 07 / 2022;
+ *  Projeto: Tetris Clone Web;
+ *  Developer: Silvanei Martins;
+ *  Email: silvaneimartins_rcc@hotmail.com;
+ *  Informacao: Arquivo principal do projeto Tetris;
+ */
 
-class Tetris {
+class Tetris { 
 	constructor(imageX, imageY, template) {
 		this.imageY = imageY;
 		this.imageX = imageX;
@@ -31,12 +38,114 @@ const squareCountX = canvas.width / size;
 const squareCountY = canvas.height / size;
 
 const shapes = [
-	new Tetris(0, 120, [
-		[0, 1, 0],
-		[0, 1, 0],
-		[1, 1, 0]
-	],
+	  new Tetris(0, 120, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [1, 1, 0],
+  ]),
+  new Tetris(0, 96, [
+    [0, 0, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+  ]),
+  new Tetris(0, 72, [
+    [0, 1, 0],
+    [0, 1, 0],
+    [0, 1, 1],
+  ]),
+  new Tetris(0, 48, [
+    [0, 0, 0],
+    [0, 1, 1],
+    [1, 1, 0],
+  ]),
+  new Tetris(0, 24, [
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+    [0, 0, 1, 0],
+  ]),
+  new Tetris(0, 0, [
+    [1, 1],
+    [1, 1],
+  ]),
+  new Tetris(0, 48, [
+    [0, 0, 0],
+    [1, 1, 0],
+    [0, 1, 1],
+  ]),
 ]
+
+
+let gameMap;
+let gameOver;
+let currentShape;
+let nextShape;
+let score;
+let initialTwoDArr;
+
+
+let gameLoop = () => {
+  setInterval(update, 1000 / gameSpeed);
+  setInverval(draw, 1000 / framePerSecond);
+}
+
+
+let update = () => {
+
+}
+
+let draw = () => {
+
+}
+
+let getRandomShape = () => {
+  return Object.create(shapes[Math.floor(Math.random() * shapes.length)]);
+}
+
+let resetVars = () => {
+  initialTwoDArr = [];
+  for (let i = 0; i < squareCountY; i ++) {
+    let temp = [];
+    for (let j = 0; j < squareCountX; j ++) {
+      temp.push({ imageX: -1, imageY: -1});
+    }
+    initialTwoDArr.push(temp)
+  }
+  score = 0;
+  gameOver = false;
+  currentShape = getRandomShape();
+  nextShape = getRandomShape();
+  gameMap = initialTwoDArr;
+}
+
+
+
+gameLoop();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
