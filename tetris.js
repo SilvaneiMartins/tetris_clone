93bc889967aa367c75929c1ebd38894d8aa38d5d
Silvanei Martins <silvaneimartins_rcc@hotmail.com>
2022-07-15 12:23:27 -0300

    Atualização do arquivo tetris.js

--- tetris.js
+++ tetris.js
@@ -6,25 +6,26 @@
  *  Informacao: Arquivo principal do projeto Tetris;
  */
 
-class Tetris { 
-	constructor(imageX, imageY, template) {
-		this.imageY = imageY;
-		this.imageX = imageX;
-		this.template = template;
-	
-	}
+class Tetris {
+  constructor(imageX, imageY, template) {
+    this.imageY = imageY;
+    this.imageX = imageX;
+    this.template = template;
+  }
 
-	checkBottom() {}
+  checkBottom() {}
 
-	checkLeft() {}
+  checkLeft() {}
 
-	moveRight() {}
+  checkRight() {}
 
-	moveLeft() {}
+  moveRight() {}
 
-	moveBottom() {}
+  moveLeft() {}
 
-	changeRotation() {}
+  moveBottom() {}
+
+  changeRotation() {}
 }
 
 const imageSquareSize = 24;
@@ -32,13 +33,17 @@ const size = 40;
 const framePerSecond = 24;
 const gameSpeed = 5;
 const canvas = document.getElementById("canvas");
+const nextShapeCanvas = document.getElementById("nextShapeCanvas");
+const scoreCanvas = document.getElementById("scoreCanvas");
 const image = document.getElementById("image");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
+const nctx = nextShapeCanvas.getContext("2d");
+const sctx = scoreCanvas.getContext("2d");
 const squareCountX = canvas.width / size;
 const squareCountY = canvas.height / size;
 
 const shapes = [
-	  new Tetris(0, 120, [
+  new Tetris(0, 120, [
     [0, 1, 0],
     [0, 1, 0],
     [1, 1, 0],
@@ -68,13 +73,13 @@ const shapes = [
     [1, 1],
     [1, 1],
   ]),
+
   new Tetris(0, 48, [
     [0, 0, 0],
     [1, 1, 0],
     [0, 1, 1],
   ]),
-]
-
+];
 
 let gameMap;
 let gameOver;
@@ -82,49 +87,46 @@ let currentShape;
 let nextShape;
 let score;
 let initialTwoDArr;
-let whiteLineTickness = 4;
-
+let whiteLineThickness = 4;
 
 let gameLoop = () => {
   setInterval(update, 1000 / gameSpeed);
-  setInverval(draw, 1000 / framePerSecond);
-}
+  setInterval(draw, 1000 / framePerSecond);
+};
 
-
-let update = () => {
-
-}
+let update = () => {};
 
 let drawRect = (x, y, width, height, color) => {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, width, height);
-}
+};
 
 let drawBackground = () => {
-  drawRect(0, 0, canvas.width, canvas.height, "#BCA0DC")
-  
-  for (let i = 0; i < squareCountX + 1; i ++) {
+  drawRect(0, 0, canvas.width, canvas.height, "#bca0dc");
+  for (let i = 0; i < squareCountX + 1; i++) {
     drawRect(
+      size * i - whiteLineThickness,
       0,
-      size * i - whiteLineThickness, 
-      canvas.height, 
+      whiteLineThickness,
+      canvas.height,
       "white"
     );
   }
 
-  for (let i = 0; i < squareCountY + 1, i ++) {
+  for (let i = 0; i < squareCountY + 1; i++) {
     drawRect(
       0,
       size * i - whiteLineThickness,
       canvas.width,
-      "white",
+      whiteLineThickness,
+      "white"
     );
   }
-}
+};
 
 let drawCurrentTetris = () => {
-  for (let i = 0; i < currentShape.template.length; i ++) {
-    for (let j = 0; j < currentShape.template.length; j ++) {
+  for (let i = 0; i < currentShape.template.length; i++) {
+    for (let j = 0; j < currentShape.template.length; j++) {
       if (currentShape.template[i][j] == 0) continue;
       ctx.drawImage(
         image,
@@ -139,12 +141,13 @@ let drawCurrentTetris = () => {
       );
     }
   }
-}
+};
 
-let drawSquares = () => {}
+let drawSquares = () => {};
 
-let drawNextShape = () => {}
+let drawNextShape = () => {};
 
+let drawGameOver = () => {};
 
 let draw = () => {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -152,26 +155,25 @@ let draw = () => {
   drawSquares();
   drawCurrentTetris();
   drawNextShape();
+  drawScore();
   if (gameOver) {
     drawGameOver();
   }
-}
+};
 
 let getRandomShape = () => {
   return Object.create(shapes[Math.floor(Math.random() * shapes.length)]);
-}
+};
 
 let resetVars = () => {
   initialTwoDArr = [];
-
-  for (let i = 0; i < squareCountY; i ++) {
+  
+  for (let i = 0; i < squareCountY; i++) {
     let temp = [];
-
-    for (let j = 0; j < squareCountX; j ++) {
-      temp.push({ imageX: -1, imageY: -1});
+    for (let j = 0; j < squareCountX; j++) {
+      temp.push({ imageX: -1, imageY: -1 });
     }
-
-    initialTwoDArr.push(temp)
+    initialTwoDArr.push(temp);
   }
 
   score = 0;
@@ -179,41 +181,7 @@ let resetVars = () => {
   currentShape = getRandomShape();
   nextShape = getRandomShape();
   gameMap = initialTwoDArr;
-}
+};
 
 resetVars();
 gameLoop();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
